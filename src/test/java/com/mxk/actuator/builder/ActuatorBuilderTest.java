25ca2206da0f4ce397d8782a26237136fa15415b
martykells <martykells@gmail.com>
2016-11-28 08:48:00 +0000

    Unit tests for ActuatorBuilder

--- src/test/java/com/mxk/actuator/builder/ActuatorBuilderTest.java
+++ src/test/java/com/mxk/actuator/builder/ActuatorBuilderTest.java
@@ -12,6 +12,7 @@ import jenkins.model.Jenkins;
 import junit.framework.TestCase;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,21 +22,33 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.File;
+import java.io.PrintStream;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.startsWith;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({Jenkins.class, StepDescriptor.class})
+@PrepareForTest({Jenkins.class, StepDescriptor.class, ActuatorBuilder.class})
 public class ActuatorBuilderTest extends TestCase {
 
     @Mock
     private Jenkins jenkins;
-
     @Mock
-    private HttpClient client;
+    private HttpClient mockHttpClient;
+    @Mock
+    private GetMethod mockGetMethod;
+    @Mock
+    private Run mockRun;
+    @Mock
+    private Launcher mockLauncher;
+    @Mock
+    private TaskListener mockTaskListener;
+
+    private FilePath mockFilePath = new FilePath(new File(""));
 
     @Before
     public void setUp() throws Exception {
@@ -51,15 +64,26 @@ public class ActuatorBuilderTest extends TestCase {
         assertEquals("Invoke Actuator", descriptor.getDisplayName());
     }
 
+    @Test
+    public void testPerform() throws Exception {
+        String url = "http://localhost/health";
+        when(mockGetMethod.getResponseBodyAsString()).thenReturn("{\"status\": \"UP\"}");
+        PowerMockito.whenNew(GetMethod.class).withArguments(url).thenReturn(mockGetMethod);
+        when(mockHttpClient.executeMethod(any(HttpMethod.class))).thenReturn(200);
+        ActuatorBuilder actuatorBuilder = new ActuatorBuilder(url);
+        actuatorBuilder.setClient(mockHttpClient);
+        PrintStream out = mock(PrintStream.class);
+        when(mockTaskListener.getLogger()).thenReturn(out);
+
+        actuatorBuilder.perform(mockRun, mockFilePath, mockLauncher, mockTaskListener);
+
+        verify(out).println("Health check response: http://localhost/health UP");
+    }
+
     @Test(expected = AbortException.class)
     public void testPerform_throws_AbortException() throws Exception {
-        ActuatorBuilder actuatorBuilder = new ActuatorBuilder("http://localhost/health");
-        Run mockRun = mock(Run.class);
-        FilePath mockFilePath = new FilePath(new File(""));
-        Launcher mockLauncher = mock(Launcher.class);
-        TaskListener mockTaskListener = mock(TaskListener.class);
-        HttpClient mockHttpClient = mock(HttpClient.class);
         when(mockHttpClient.executeMethod(any(HttpMethod.class))).thenReturn(404);
+        ActuatorBuilder actuatorBuilder = new ActuatorBuilder("http://localhost/health");
         actuatorBuilder.setClient(mockHttpClient);
 
         actuatorBuilder.perform(mockRun, mockFilePath, mockLauncher, mockTaskListener);
